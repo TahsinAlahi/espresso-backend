373ba98ca58073dae97cf3d40f5b490a1a503d21
Tahsin <alahitahsin@gmail.com>
2024-11-23 03:58:30 +0600

    updated: database

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require("dotenv/config");
-const express = require("express");
-const app = express();
-
+const mongoose = require("mongoose");
+const app = require("./app");
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected");
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
