dfb3900dc199f510a232ab219c3ad1808685952a
Tahsin <alahitahsin@gmail.com>
2024-11-25 21:03:52 +0600

    created: update database info

--- middlewares/coffee.js
+++ middlewares/coffee.js
@@ -49,8 +49,37 @@ async function getCoffee(req, res, next) {
   }
 }
 
+async function updateCoffee(req, res, next) {
+  const { id } = req.params;
+  try {
+    if (!mongoose.isValidObjectId(id)) throw createHttpError(400, "Invalid id");
+
+    const { name, chef, supplier, taste, category, details } = req.body;
+
+    const updatedCoffee = await coffeeModel.findByIdAndUpdate(
+      id,
+      {
+        ...(name && { name }),
+        ...(chef && { chef }),
+        ...(supplier && { supplier }),
+        ...(taste && { taste }),
+        ...(category && { category }),
+        ...(details && { details }),
+      },
+      { returnOriginal: false }
+    );
+
+    if (!updatedCoffee) throw createHttpError(404, "Coffee not found");
+
+    res.status(200).json(updatedCoffee);
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   getAllCoffees,
   createCoffee,
   getCoffee,
+  updateCoffee,
 };
--- routes/coffees.js
+++ routes/coffees.js
@@ -4,12 +4,13 @@ const {
   getAllCoffees,
   createCoffee,
   getCoffee,
+  updateCoffee,
 } = require("../middlewares/coffee");
 
 // get all the coffees
 router.route("/").get(getAllCoffees).post(createCoffee);
 
 // get a specific coffee
-router.route("/:id").get(getCoffee);
+router.route("/:id").get(getCoffee).patch(updateCoffee);
 
 module.exports = router;
